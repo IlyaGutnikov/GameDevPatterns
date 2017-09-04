b72da93e897f45477e8c86049207ea99bac9e316
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2017-09-04 23:38:22 +0300

    Паттерн Объект-Тип

---
+++ PatternTypeObject/PatternTypeObject/Breed.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PatternTypeObject
+{
+    
+    /// <summary>
+    /// Получает параметры здоровья и атаки из JSON\XML
+    /// </summary>
+    public class Breed
+    {
+        private int health;
+        private string attack;
+        
+        public Breed(Breed _parent, int _health, string _attack)
+        {
+            if (_parent != null)
+            {
+                if (_health == 0)
+                    health = _parent.GetHealth();
+
+                if (String.IsNullOrEmpty(_attack))
+                    attack = _parent.GetAttack();
+            }
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        public string GetAttack()
+        {
+            return attack;
+        }
+    }
+}
---
+++ PatternTypeObject/PatternTypeObject/Monster.cs
@@ -0,0 +1,25 @@
+﻿namespace PatternTypeObject
+{
+    public class Monster
+    {
+        private Breed breed;
+
+        private int health;
+        
+        /// <summary>
+        /// Создается на основе данных
+        /// </summary>
+        /// <param name="_breed"></param>
+        public Monster(Breed _breed)
+        {
+            breed = _breed;
+            health = _breed.GetHealth();
+        }
+
+        public string GetAttack()
+        {
+            return breed.GetAttack();
+        }
+        
+    }
+}
